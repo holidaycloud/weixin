e4384c92395a81a63bc8883706bcc1e91696745c
zzy <zzy@holidaycloud.cn>
2014-10-30 10:50:34 +0800

    port

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500).end();
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3333);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
