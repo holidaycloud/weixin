059c737486e57242e19a8aa187e2a77756fa90f5
zzy <zzy@holidaycloud.cn>
2014-11-25 14:02:09 +0800

    msg

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.log(err);
         res.status(err.status || 500).end();
     });
 }
@@ -71,6 +72,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    console.log(err);
     res.status(err.status || 500).end();
 });
 
